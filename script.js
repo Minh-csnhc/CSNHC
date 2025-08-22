29eb07e57a96552575d76702a6a79e14d83cb90d
Minh-csnhc <ducminhnhc2010@gmail.com>
2025-08-23 01:45:05 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-window.addEventListener("scroll", function() {
+const menuIcon = document.getElementById("menu-icon");
+const dropdown = document.getElementById("dropdown-menu");
+
+// Scroll effect
+window.addEventListener("scroll", function () {
     const navbar = document.querySelector(".navbar");
     if (window.scrollY > 50) {
         navbar.classList.add("scrolled");
     } else {
         navbar.classList.remove("scrolled");
+    }
+});
+
+// Toggle dropdown khi click
+menuIcon.addEventListener("click", () => {
+    if (dropdown.classList.contains("show")) {
+        dropdown.style.maxHeight = null;
+        dropdown.classList.remove("show");
+    } else {
+        dropdown.style.maxHeight = dropdown.scrollHeight + "px"; // mở ra
+        dropdown.classList.add("show");
+    }
+});
+
+// Auto remove khi resize về desktop
+window.addEventListener("resize", () => {
+    if (window.innerWidth > 1243) {
+        dropdown.style.maxHeight = null;
+        dropdown.classList.remove("show");
     }
 });
